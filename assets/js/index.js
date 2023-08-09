e4062314d6d89bd445c3ea1986d58a16260f5ef6
Adrastea <Adrastea966@yahoo.com>
2023-08-09 17:20:27 -0300

    Nuevo contenido y eliminacion de páginas ya no necesarias

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,12 @@
-//Dark - light mode 
+//Botón scroll arriba
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  });
+}
+
+//Modo oscuro-claro 
 const toggleMode = () => {
   const slider = document.querySelector('.mode');
   const moonIcon = document.querySelector('.uil-moon');
@@ -230,5 +238,33 @@ arrowIn.addEventListener('click', () => {
       arrow.style.display = 'none';
     });
     arrowIn.classList.remove('rotate');
+  }
+});
+
+//formulario de contacto
+const $form = document.querySelector('#form');
+const $buttonMailTo = document.querySelector('#mail-to')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailTo.setAttribute('href',
+      `mailto:ballian.eliana@gmail.com?subject= Nombre: ${form.get('name')} Correo: ${form.get('email')}&body=${form.get('message')}`
+  )
+  $buttonMailTo.click()
+}
+//scroll seccion nosotros
+window.addEventListener('scroll', function () {
+  let nosotrosSection = document.getElementById('nosotros');
+  let scrollTopButton = document.querySelector('.scroll-top-button');
+
+  let nosotrosSectionPosition = nosotrosSection.getBoundingClientRect().top;
+
+  if (nosotrosSectionPosition <= 0) {
+      scrollTopButton.classList.add('show');
+  } else {
+      scrollTopButton.classList.remove('show');
   }
 });
